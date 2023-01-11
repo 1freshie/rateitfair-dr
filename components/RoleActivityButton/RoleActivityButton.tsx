92474192fe7f204f7dc3251d5a3a7726e3a92e5a
1freshie <hristian.todorov14@gmail.com>
2023-01-11 08:42:34 +0200

    Fixed text on the Role Activity Button component

--- components/RoleActivityButton/RoleActivityButton.tsx
+++ components/RoleActivityButton/RoleActivityButton.tsx
@@ -12,7 +12,7 @@ export default function RoleActivityButton({
     <div className="w-full lg:w-[500px] p-6 mt-16 lg:mt-20 flex justify-between items-center font-medium text-base md:text-lg lg:text-xl xl:text-2xl text-secondary--gray border border-secondary--gray rounded-[30px] duration-300 hover:text-primary--blue hover:border-primary--blue cursor-pointer">
       {userRole === "Admin" && (
         <>
-          <p>Add a new company...</p>
+          <p>Add a new organization...</p>
           <FontAwesomeIcon
             icon={faPlus}
             className="w-6 md:w-7 lg:w-8 xl:w-9 h-6 md:h-7 lg:h-8 xl:h-9"
